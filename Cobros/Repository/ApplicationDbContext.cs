51644d0086ec9832e24c2fcafdaeeeea8c1ff366
CHRIS ANTHONY CAUICH POLANCO <21090144@alumno.utmetropolitana.edu.mx>
2024-01-16 10:02:06 -0600

    Add project files.

---
+++ Cobros/Repository/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.Contracts;
+using System.IO;
+using Cobros.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Cobros.Repository
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        //public DbSet<Cliente> Clientes { get; set; }
+
+
+
+    }
+}
